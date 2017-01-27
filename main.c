68b136cf7166f1008cfb49a51bf6dc98665a8117
Andrew Hill <andrew.2.hill@ucdenver.edu>
2017-01-26 22:01:21 -0700

    Added some basic code and a layout for the program.

--- main.c
+++ main.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/*
+ * A struct definition for a single node in our binary tree.
+ */
+struct node {
+    char* word;
+    int count;
+    struct node* left_child;
+    struct node* right_child;
+};
 
-int main() {
+int main(int argc, char** argv) {
+    // Check for correct number of arguments.
+    if (argc == 1 || argc > 2){
+        printf("Incorrect number of arguments. Please supply a single filename argument.\n");
+        exit(0);
+    }else {
+        printf("Attempting to open %s\n",argv[1]);
+        // Pointer to the input file
+        FILE *input;
+        // Open the file in read mode.
+        input = fopen(argv[1],"r");
+        // Check if file open was successful
+        if (input == NULL){
+            printf("Failed to open file \'%s\', please check that the file exists.\n",argv[1]);
+            exit(0);
+        }
+    }
+
+    // Create Head node for binary tree.
+    struct node* head = malloc(sizeof(struct node));
+
+
 
 }
---
+++ main.h
@@ -0,0 +1,7 @@
+//
+// Created by Andrew on 1/26/2017.
+//
+
+#ifndef WORDTREE_MAIN_H
+#define WORDTREE_MAIN_H
+#endif //WORDTREE_MAIN_H
